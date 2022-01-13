32c29de48bbef0a780f221d82daa7e298fb24e48
EZZAYANI Nassim <nassim.ezzayani@ecole.ensicaen.fr>
2022-01-13 18:05:21 +0100

    Upload New File

---
+++ app-entreprise/Result.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Text, StyleSheet, View } from 'react-native'
+
+export default class Message extends Component {
+    render() {
+        return (
+            <View>
+                <Text> Result screen </Text>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({})
